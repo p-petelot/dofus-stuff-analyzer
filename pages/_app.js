a2975c1b6fd0d5bf737f9ce858afce68445fee9c
Pierre PETELOT <pierre--petelot@hotmail.fr>
2025-10-16 14:19:07 +0200

    Align theme imports and accent focus states

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 // pages/_app.js
+import "../public/tokens.css";
 import "../public/styles.css";
+import "../public/overlay.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
