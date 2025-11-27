1827a9aa0333218c8896e6415cff6fcb84a635f7
Pierre PETELOT <pierre--petelot@hotmail.fr>
2025-11-27 10:36:57 +0100

    Refine inspiration layout and modal polish

--- pages/index.js
+++ pages/index.js
@@ -8955,7 +8955,7 @@ export default function Home({
                                               ) : (
                                                 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                   <path
-                                                    d="M15 3h6v6m0-6-7.5 7.5m1.5-1.5a6 6 0 1 1-12 0 6 6 0 0 1 12 0Z"
+                                                    d="M14 3h7v7m-7 0 3.25-3.25M11 6a6 6 0 1 0 0 12 6 6 0 0 0 0-12Z"
                                                     stroke="currentColor"
                                                     strokeWidth="1.6"
                                                     strokeLinecap="round"
