207b50b4885182662d75353ada69d430e9f2dc28
Pierre PETELOT <pierre--petelot@hotmail.fr>
2025-12-05 14:55:31 +0100

    Add fallback data for cosmetics news

---
+++ data/dofusNewsFallback.js
@@ -0,0 +1,58 @@
+const fallbackNews = [
+  {
+    link: "https://www.dofus.com/fr/mmorpg/actualites/news/1766328-dungeon-rusher-trois-defis-ile-nowel",
+    title: "Dungeon Rusher : trois défis sur l’Île de Nowel !",
+    image: "https://static.ankama.com/ankama/cms/images/273/2025/11/26/1766423_69271b7c2a382.w500h.jpg",
+    descriptionHtml:
+      "Pour les fêtes, le Dungeon Rusher voit les choses en grand : non pas un, ni deux, mais <em>trois</em> donjons à affronter en famille ou entre amis ! Et <strong>trois boucliers d’apparat</strong> à la clé pour nos amis rusheurs. De quoi vous occuper jusqu’à la nouvelle année !",
+    publishedAt: "2025-11-27T12:00:00.000Z",
+    publishedAtText: "27 Novembre 2025",
+  },
+  {
+    link: "https://www.dofus.com/fr/mmorpg/actualites/news/1758901-packs-cosmetiques-bouclier-clash-2025",
+    title: "Packs cosmétiques : Bouclier du Clash 2025",
+    image: "https://static.ankama.com/ankama/cms/images/273/2025/07/11/1758901_packclash.w500h.jpg",
+    descriptionHtml:
+      "Un pack complet avec un <strong>bouclier d’apparat</strong>, une <em>emote exclusive</em> et un titre unique pour célébrer le Clash 2025.",
+    publishedAt: "2025-07-11T12:00:00.000Z",
+    publishedAtText: "11 Juillet 2025",
+  },
+  {
+    link: "https://www.dofus.com/fr/mmorpg/actualites/news/1754020-harnachements-wakfu",
+    title: "Nouveaux harnachements Wakfu",
+    image: "https://static.ankama.com/ankama/cms/images/273/2025/05/03/1754020_wakfu_mounts.w500h.jpg",
+    descriptionHtml:
+      "Découvrez les harnachements inspirés de la série <strong>Wakfu</strong> pour vos montures, disponibles en jeu et dans la boutique.",
+    publishedAt: "2025-05-03T12:00:00.000Z",
+    publishedAtText: "3 Mai 2025",
+  },
+  {
+    link: "https://www.dofus.com/fr/mmorpg/actualites/news/1749100-pack-nowel-2024",
+    title: "Pack de Nowel 2024 : capes, coiffes et familiers",
+    image: "https://static.ankama.com/ankama/cms/images/273/2024/12/08/1749100_packnowel.w500h.jpg",
+    descriptionHtml:
+      "Le pack de Nowel revient avec une <em>cape scintillante</em>, une <strong>coiffe enneigée</strong> et un familier givré pour accompagner vos aventures.",
+    publishedAt: "2024-12-08T12:00:00.000Z",
+    publishedAtText: "8 Décembre 2024",
+  },
+  {
+    link: "https://www.dofus.com/fr/mmorpg/actualites/news/1745012-nouveaux-boucliers-apparat",
+    title: "Nouveaux boucliers d’apparat",
+    image: "https://static.ankama.com/ankama/cms/images/273/2024/09/21/1745012_shields.w500h.jpg",
+    descriptionHtml:
+      "De nouveaux <strong>boucliers d’apparat</strong> débarquent en boutique : collectionnez-les pour personnaliser vos combats !",
+    publishedAt: "2024-09-21T12:00:00.000Z",
+    publishedAtText: "21 Septembre 2024",
+  },
+  {
+    link: "https://www.dofus.com/fr/mmorpg/actualites/news/1740010-emotes-ankama-launcher",
+    title: "Émotes exclusives Ankama Launcher",
+    image: "https://static.ankama.com/ankama/cms/images/273/2024/06/10/1740010_emotes.w500h.jpg",
+    descriptionHtml:
+      "Connectez-vous via l’<strong>Ankama Launcher</strong> pour récupérer deux émotes inédites et montrer votre style en jeu.",
+    publishedAt: "2024-06-10T12:00:00.000Z",
+    publishedAtText: "10 Juin 2024",
+  },
+];
+
+export default fallbackNews;
